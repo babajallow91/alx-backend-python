946e604db6b49f58c576b2f4a9618b90753d6d56
babajallow91 <momodousalieu.jallow@gmail.com>
2023-07-10 23:51:28 +0000

    hpyt

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,8 @@
 import asyncio
 import random
 
-async def wait_random(max_delay: int = 10) -> float:
 
+async def wait_random(max_delay: int = 10) -> float:
     """Waits for a random number of seconds.
     """
     wait_time = random.random() * max_delay
